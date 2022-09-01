06ddaa53754a50a7eba7d6705aa340cd3182ac60
Aziz Hussain <56464631+Aziz-code@users.noreply.github.com>
2022-09-01 13:52:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { CssBaseline, MuiThemeProvider } from "@material-ui/core";
+import React from "react";
+import Content from "./component/content";
+import Footer from "./component/footer";
+import Header from "./component/header";
+import { createMuiTheme } from "@material-ui/core";
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#007bff",
+    },
+  },
+  typography: {
+    h5: {
+      fontSize: 20,
+    },
+    h6: {
+      fontSize: 14,
+    },
+    body1: {
+      fontSize: 14,
+    },
+    subtitle1: {
+      fontSize: 12,
+    },
+  },
+});
+function App() {
+  return (
+    <>
+      <MuiThemeProvider theme={theme}>
+        <Header />
+        <main>
+          <Content />
+        </main>
+        <Footer />
+        <CssBaseline />
+      </MuiThemeProvider>
+    </>
+  );
+}
+
+export default App;
